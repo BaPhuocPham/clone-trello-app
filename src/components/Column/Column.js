2c53ea03e63dc2a940b5592f14f05289d1f66980
“BaPhuoc” <phuoc.18112002@gmail.com>
2023-02-09 16:57:59 +0700

    Handle State change when drag and drop

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -7,12 +7,8 @@ import { mapOrder } from "../../utilities/sort";
 
 function Column(props) {
 
-  const { column } = props
+  const { column, onCardDrop } = props
   const cards = mapOrder(column.cards, column.cardOrder, 'id')
-
-  const onCardDrop = (dropResult) => {
-    console.log(dropResult)
-  }
 
   return (
     <div className='column'>
@@ -21,7 +17,7 @@ function Column(props) {
       <Container
         orientation="vertical" //default
         groupName="col"
-        onDrop={onCardDrop}
+        onDrop={dropResult => onCardDrop(column.id, dropResult)}
         getChildPayload={index => cards[index]}
         dragClass="card-ghost"
         dropClass="card-ghost-drop"
@@ -39,7 +35,11 @@ function Column(props) {
         ))}
         </Container>
       </div>
-      <footer>Add another card</footer>
+      <footer>
+        <div className="footer-actions">
+          <i className="fa fa-plus icon" /> Add another card
+        </div>
+      </footer>
     </div>    
   )
 }
